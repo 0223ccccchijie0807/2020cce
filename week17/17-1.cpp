cfe6f3f513cb9bd5f2667f647e8281200a4d90a3
0223ccccchijie0807 <79628377+0223ccccchijie0807@users.noreply.github.com>
2021-06-17 17:32:49 +0800

    Add files via upload

---
+++ week17/17-1.cpp
@@ -0,0 +1,12 @@
+void setup()
+{
+    size(400,200);
+    textSize(40);
+}
+String line="hello";//字串
+void draw()
+{
+    background(#718BC1);
+    text(line,100,100);//可以將字串line畫出來
+    text("World",100,150);
+}
---
+++ week17/17-4.cpp
@@ -0,0 +1,15 @@
+void setup()
+{
+    size(400,200);
+    textSize(40);
+}
+int x=100,y=100;
+void draw()
+{
+  background(#718BC1);
+  rect(x,y,50,50);
+}
+void keyPressed(){
+  if(keyCode==LEFT) x-=10;
+  if(keyCode==RIGHT) x+=10;
+}
